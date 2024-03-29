03017d6d75c6c61b4e256c7e2926a5db5a4dcb7a
Sam <sambolton.kw@gmail.com>
2024-03-29 16:17:03 -0400

    geoapi test worked

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,12 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lat=38&lon=-77&units=imp
     console.log(data);
   });
 
-
+  'http://api.openweathermap.org/geo/1.0/direct?q=Baltimore&limit=1&appid=1b770f237929bb21c34412c090d06664'
+  // AJAX call requires a third party library, jQuery lesson 5
+$.ajax({
+    url: requestUrl,
+    method: 'GET',
+  }).then(function (response) {
+    console.log('Ajax Response \n-------------');
+    console.log(response);
+  });
