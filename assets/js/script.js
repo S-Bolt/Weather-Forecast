4a7823ebc046b8bb12b728c3110499a7452e643c
Sam <sambolton.kw@gmail.com>
2024-11-03 15:18:57 -0500

    refrator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 const apiKey = '1b770f237929bb21c34412c090d06664';
+
 let searchForm = $("#citySearchForm");
 let cityInput = $("#cityInputLocation")
 let searchButton = $("#search");
 let currentWeatherContainer = $("#currentWeatherContainer");
 let fiveDayContainer = $("#5day");
-let savedCityButton = $("#savedCityButton");
+let searchedCityButton = $("#savedCityButton");
+
+searchButton.on('click', handleSearchCity);
+
+// Load saved cities on page load
+loadSavedCities();
+
 //handle search city submission event 
 function handleSearchCity(event){
     event.preventDefault();
@@ -14,17 +21,46 @@ function handleSearchCity(event){
 
     if (cityName) {
       getGeoData(cityName);
-
-      //searchForm.textContent ='';
-      //cityInput.textContent = '';
+      saveCity(cityName);
 
     } else {
       alert("Please enter city name")
     }                              
 };
 
-// Function to converty city name to geo data
+// Save city to localStorage
+function saveCity(cityName){
+  let cities = JSON.parse(localStorage.getItem('cities')) || []
 
+  if (!cities.includes(cityName)) {
+    cities.push(cityName);
+    localStorage.setItem('cities', JSON.stringify(cities));
+    renderSavedCityButton(cityName);
+  }
+}
+
+// Load saved cities from localStorage
+function loadSavedCities() {
+  let cities = JSON.parse(localStorage.getItem('cities')) || [];
+  cities.forEach(city => {
+    renderSavedCityButton(city);
+  });
+}
+
+// Render saved city button
+function renderSavedCityButton(cityName) {
+  let cityButton = $('<button>')
+    .addClass('btn btn-secondary w-100 my-1')
+    .text(cityName)
+    .on('click', function() {
+      getGeoData(cityName);
+    });
+
+  searchedCityButton.append(cityButton);
+}
+
+
+// Function to converty city name to geo data
 function getGeoData(cityName) {
     let geoApiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=1b770f237929bb21c34412c090d06664';
     
@@ -90,13 +126,16 @@ if (!currentWeather){
   alert("No current weather availale");
   return;
 };
-  let currentCard = $('<div>').text(currentWeather.name).css({'font-size': '24px', 'font-weight': 'bold'});
-  let icon = $('<i>').attr('id', 'weather-icon').addClass('bi bi-brightness-high-fill');
+  let currentCard = $('<div>').addClass('current-card');
+  let cityName = $('<h2>').text(currentWeather.name);
+  let iconUrl = `https://openweathermap.org/img/wn/${currentWeather.weather[0].icon}@2x.png`;
+  let icon = $('<img>').attr('src', iconUrl).addClass('weather-icon');
+  let description = $('<h4>').text(currentWeather.weather[0].description);
   let temp = $('<h5>').text('Temp: ' + currentWeather.main.temp + ' F');
   let humid = $('<h5>').text('Humidity: ' + currentWeather.main.humidity + ' %');
   let wind = $('<h5>').text('Wind Speed: ' + currentWeather.wind.speed + ' mph');
 
-  currentCard.append(icon, temp, humid, wind);
+  currentCard.append(cityName, icon, description, temp, humid, wind);
   currentWeatherContainer.append(currentCard);
 
 //need to come back and add date, icon, and figure out how to make icon switch
@@ -118,18 +157,28 @@ function renderFiveDay(fiveDay){
     };
 
   console.log(fiveDayForecast);
+
+   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
      //itterate through new array, fiveDayForecast and append
   for (let i = 0; i <fiveDayForecast.length; i++){
 
-    let fiveDayCard = $("<div>").addClass("flex-grow-1 bg-info mx-3");
-    let icon = $('<i>').attr('id', 'weather-icon').addClass('bi bi-brightness-high-fill');
-    let date = $('<h4>').text(fiveDayForecast[i].dt_txt.split(' ')[0]);
+    // Get the date from the API data and convert it to a day of the week
+    let dateStr = fiveDayForecast[i].dt_txt.split(' ')[0]; 
+    let date = new Date(dateStr);
+    let daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let dayOfWeek = daysOfWeek[date.getDay()];
+
+    let fiveDayCard = $("<div>").addClass("five-day-card flex-grow-1 mx-3");
+    let iconUrl = `https://openweathermap.org/img/wn/${fiveDayForecast[i].weather[0].icon}@2x.png`;
+    let icon = $('<img>').attr('src', iconUrl).addClass('weather-icon');
+    let dayName = $('<h4>').text(dayOfWeek);
+    let description = $('<h5>').text(fiveDayForecast[i].weather[0].description);
     let temp = $('<h5>').text('Temp: ' + fiveDayForecast[i].main.temp + ' F');
     let humid = $('<h5>').text('Humidity: ' + fiveDayForecast[i].main.humidity + ' %');
     let wind = $('<h5>').text('Wind Speed: ' + fiveDayForecast[i].wind.speed + ' mph');
 
       //append children to fiveDayCard then append car to containter
-      fiveDayCard.append(date, icon, temp, humid, wind);
+      fiveDayCard.append(dayName, icon, description, temp, humid, wind);
       fiveDayContainer.append(fiveDayCard);
  };
 };
