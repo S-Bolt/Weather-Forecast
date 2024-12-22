8adfeeb9e65b04c2bf557c95058437be03120301
Sam <sambolton.kw@gmail.com>
2024-12-22 09:58:22 -0500

    geolocation for user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,30 @@ document.addEventListener('DOMContentLoaded', () => {
   updateBackground('default'); 
 });
 
+//Get user location'
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  const crd = pos.coords;
+  console.log(crd)
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+  getWeatherData(crd.latitude, crd.longitude)
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options)
+
+
 searchForm.on('submit', handleSearchCity);
 
 // Load saved cities on page load
